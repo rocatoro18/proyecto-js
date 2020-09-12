7fdb2dfba5e82ec9bfea8d5598e0924f78bfc792
Carlos Torres <59813315+rocatoro18@users.noreply.github.com>
2020-09-11 22:14:12 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+  // Slider
+
     $('.galeria').bxSlider({
         mode: 'fade',
         captions: true,
@@ -6,4 +9,48 @@ $(document).ready(function(){
         responsive: true,
         pager: true
       });
+
+  // Posts
+
+  var posts = [
+    {
+      title: 'Prueba de titulo 1',
+      date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dapibus, enim quis fermentum rhoncus, dolor erat pellentesque mauris, ut molestie sem ex at sem.'
+    },
+    {
+      title: 'Prueba de titulo 2',
+      date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dapibus, enim quis fermentum rhoncus, dolor erat pellentesque mauris, ut molestie sem ex at sem.'
+    },
+    {
+      title: 'Prueba de titulo 3',
+      date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dapibus, enim quis fermentum rhoncus, dolor erat pellentesque mauris, ut molestie sem ex at sem.'
+    },
+    {
+      title: 'Prueba de titulo 4',
+      date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dapibus, enim quis fermentum rhoncus, dolor erat pellentesque mauris, ut molestie sem ex at sem.'
+    },
+    {
+      title: 'Prueba de titulo 5',
+      date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dapibus, enim quis fermentum rhoncus, dolor erat pellentesque mauris, ut molestie sem ex at sem.'
+    },
+  ];
+
+  posts.forEach((item, index) => {
+    var post = `
+    <article class = "post">
+     <h2>${item.title}</h2>
+       <span class = "date">${item.date}</span>
+         <p>
+            ${item.content}
+          </p>
+        <a href = "#" class = "button-more">Leer Más</a>
+    </article>
+    `;
+    $("#posts").append(post);
+  });
 });
